706ca19b87244a8e2b61bc76d1f38abd86a77f57
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-05-07 17:14:00 -0500

    updated Notifications

--- react_intro/task_2/dashboard/src/utils.js
+++ react_intro/task_2/dashboard/src/utils.js
@@ -1,15 +1,17 @@
-// task_1/dashboard/src/utils.js
-
 function getFullYear() {
-    return new Date().getFullYear();
+  return new Date().getFullYear();
+}
+
+function getFooterCopy(isIndex) {
+  if (isIndex) {
+    return "Holberton School";
+  } else {
+    return "Holberton School main dashboard";
   }
-  
-  function getFooterCopy(isIndex) {
-    if (isIndex) {
-      return "Holberton School";
-    } else {
-      return "Holberton School main dashboard";
-    }
-  }
-  
-  export { getFullYear, getFooterCopy };
+}
+
+function getLatestNotification() {
+  return "<strong>Urgent requirement</strong> - complete by EOD";
+}
+
+export { getFullYear, getFooterCopy, getLatestNotification };
