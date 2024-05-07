8197da7b5e359ae95b94de27acedd70a3fd6ce89
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-05-07 17:06:23 -0500

    app.js updated for passwords etc

--- react_intro/task_2/dashboard/src/App.js
+++ react_intro/task_2/dashboard/src/App.js
@@ -12,6 +12,11 @@ function App() {
       </header>
       <body className="App-body">
         <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email:</label>
+        <input id="email" type="email" />
+        <label htmlFor="password">Password:</label>
+        <input id="password" type="password" />
+        <button>OK</button>
       </body>
       <footer className="App-footer">
         <p>
