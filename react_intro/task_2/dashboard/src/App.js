40091465b458c21b0a5afdd2efc46672f1331195
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-05-07 17:05:15 -0500

    copied files

---
+++ react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import holbertonLogo from './holberton-logo.jpg';
+import { getFooterCopy, getFullYear } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={holbertonLogo} className="App-logo" alt="Holberton logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+      </body>
+      <footer className="App-footer">
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy(true)}
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
