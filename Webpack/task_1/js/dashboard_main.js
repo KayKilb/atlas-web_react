b82a4e37d14eedc31a1fb93bdf2c115e31c2a0f7
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-05-07 10:56:44 -0500

    added js for dashboard, button.click may not work

---
+++ Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,18 @@
+import $ from 'jquery';
+import debounce from 'lodash/debounce';
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+const button = $('<button>Click here to get started</button>');
+$('body').append(button);
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+let count = 0;
+const updateCounter = () => {
+  count += 1;
+  $('#count').text(`${count} clicks on the button`);
+};
+
+const debounceCounter = debounce(updateCounter, 500);
+button.click(debounceCounter);
