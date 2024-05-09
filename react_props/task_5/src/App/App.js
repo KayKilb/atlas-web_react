b57064d65d34de74953ae166002b1a38fec24298
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-05-08 23:35:00 -0500

    moved files from previous to current

---
+++ react_props/task_5/src/App/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import logo from '../assets/holberton-logo.jpg'
+import { getFooterCopy, getFullYear } from '../utils/utils';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={holbertonLogo} className="App-logo" alt="Holberton logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email:</label>
+        <input id="email" type="email" />
+        <label htmlFor="password">Password:</label>
+        <input id="password" type="password" />
+        <button>OK</button>
+      </body>
+      <footer className="App-footer">
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy(true)}
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
