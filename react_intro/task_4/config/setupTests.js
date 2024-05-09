56af90f85375a86739b7f7ad371c598327ca2c73
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-05-09 11:04:52 -0500

    changes

--- react_intro/task_4/config/setupTests.js
+++ react_intro/task_4/config/setupTests.js
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom';
 import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@cfaester/enzyme-adapter-react-18';
 import toJSON from 'enzyme-to-json';
 
 configure({ adapter: new Adapter() });
