c43f4e2c67cee33c0c96747517299c2d373bca2a
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-05-11 22:07:51 -0500

    updated Notifications files

--- react_intro/task_5/dashboard/src/Notifications/Notifications.js
+++ react_intro/task_5/dashboard/src/Notifications/Notifications.js
@@ -1,29 +1,18 @@
 import React from 'react';
 import './Notifications.css';
-import { getLatestNotification } from './utils';
+import { getLatestNotification } from '../utils';
 
 function Notifications() {
-  const handleClose = () => {
-    console.log('Close button has been clicked');
-  };
-
   return (
     <div className="Notifications">
-      <p>Here is the list of notifications</p>
       <button
-        style={{
-          position: 'absolute',
-          right: '10px',
-          top: '10px',
-          backgroundColor: 'transparent',
-          border: 'none',
-          cursor: 'pointer',
-        }}
+        style={{ position: 'absolute', right: '10px', top: '10px', backgroundColor: 'transparent', border: 'none', cursor: 'pointer' }}
         aria-label="Close"
-        onClick={handleClose}
+        onClick={() => console.log('Close button has been clicked')}
       >
         x
       </button>
+      <p>Here is the list of notifications</p>
       <ul>
         <li data-priority="default">New course available</li>
         <li data-priority="urgent">New resume available</li>
