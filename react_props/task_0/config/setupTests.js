d610141241aecc9d7bca6ffcd8ea68ef79c0be07
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-05-14 13:37:49 -0500

    added the correct file that is working to react props folder

--- react_props/task_0/config/setupTests.js
+++
@@ -1,9 +0,0 @@
-import '@testing-library/jest-dom';
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-18';
-import toJSON from 'enzyme-to-json';
-
-configure({ adapter: new Adapter() });
-
-// Add a serializer for Enzyme snapshots
-expect.addSnapshotSerializer(toJSON);
