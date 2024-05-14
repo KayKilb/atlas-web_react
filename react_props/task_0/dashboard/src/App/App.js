79af7f05dfa3358326b953afc84cdf91b6fd2e45
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-05-14 13:57:14 -0500

    updated App.js to remove Headers and added a Header.js file for headers

--- react_props/task_0/dashboard/src/App/App.js
+++ react_props/task_0/dashboard/src/App/App.js
@@ -1,43 +1,20 @@
 import React from "react";
 import "./App.css";
-import { getFullYear, getFooterCopy } from "../utils/utils";
-import HolbertonLogo from "../assets/holberton_logo.jpg";
+import Notifications from "../Notifications/Notifications";
+import Header from "../Header/Header";
+import Login from "../Login/Login";
+import Footer from "../Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={HolbertonLogo} className="App-logo" alt="logo" />
-        <h1 className="header">School Dashboard</h1>
-      </header>
-      <div className="App-body">
-        <p>Login to access the full dashboard</p>
-        <div>
-          <label
-            htmlFor="email"
-            onClick={() => document.getElementById("email").focus()}
-          >
-            Email:
-          </label>
-          <input type="email" id="email" name="email" />
-
-          <label
-            htmlFor="password"
-            onClick={() => document.getElementById("password").focus()}
-          >
-            Password:
-          </label>
-          <input type="password" id="password" name="password" />
-          <button>OK</button>
-        </div>
+    <>
+      <Notifications />
+      <div className="App">
+        <Header />
+        <Login />
+        <Footer />
       </div>
-      <footer className="App-footer">
-        <p>{getFooterCopy(false)}</p>
-        <p>
-          Copyright {getFullYear()} - {getFooterCopy(true)}
-        </p>
-      </footer>
-    </div>
+    </>
   );
 }
 
