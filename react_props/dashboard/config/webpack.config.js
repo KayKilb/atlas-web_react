6be31161be0631e5aac94c3881e8e9003c9cecd0
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-05-14 14:56:01 -0500

    created copy of dashboard

---
+++ react_props/dashboard/config/webpack.config.js
@@ -0,0 +1,70 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "../dist"),
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "assets/images",
+            },
+          },
+          {
+            loader: "image-webpack-loader",
+            options: {
+              mozjpeg: {
+                progressive: true,
+              },
+              optipng: {
+                enabled: false,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.template.html",
+      inject: "body",
+      filename: "index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  devtool: "source-map",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "../dist"),
+    },
+    compress: true,
+    hot: true,
+  },
+};
